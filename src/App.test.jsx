002136c59f9e9755089d97baa197689362d2fecf
Asko <hulkko54@gmail.com>
2025-02-20 11:27:39 +0200

    Document nr3

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { describe, it, expect, afterEach } from 'vitest';
 import '@testing-library/jest-dom/extend-expect';
 import App from './App';
+import { createMockServer } from './createMockServer';
 
 describe('Weather app tests', () => {
+    let server;
+    beforeEach(() => {
+        server = createMockServer();
+    })
+    afterEach(() => {
+        server.shutdown();
+    })
     it('renders weather application title', () => {
         render(<App />);
         const linkElement = screen.getByText(/Weather Application/i);
         expect(linkElement).toBeInTheDocument();
+    });
+
+    it('shows city search results', async () => {
+        render(<App />);
+
+        const input = screen.getByTestId('seacrh-input')
+        userEvent.type(input, 'Melbourne')
+
+        const button = screen.getByTestId('search-button')
+        userEvent.click(button)
+
+        await waitFor(() => expect(screen.getAllByText(/Melbourne/i).length).toEqual(5))
     });
 });
